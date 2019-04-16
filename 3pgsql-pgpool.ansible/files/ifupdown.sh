aa1d729b9059c898e614f5f443597e3b92c00f35
TAKATSUKA Haruka <harukat@sraoss.co.jp>
2019-04-16 18:57:03 +0900

    Support escalation command.

--- 3pgsql-pgpool.ansible/files/ifupdown.sh
+++ 3pgsql-pgpool.ansible/files/ifupdown.sh
@@ -12,7 +12,7 @@ then
   /usr/bin/aws --region $RGN ec2 assign-private-ip-addresses \
     --network-interface-id $ENI --private-ip-addresses $ADDR
   /sbin/ip addr add ${ADDR}/${MSK} dev $DEV
-elif ["x$CMD" = "xdown" ]
+elif [ "x$CMD" = "xdown" ]
 then 
   /sbin/ip addr del ${ADDR}/${MSK} dev $DEV
   /usr/bin/aws --region $RGN ec2 unassign-private-ip-addresses \
